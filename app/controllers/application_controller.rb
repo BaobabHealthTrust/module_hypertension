de31b8e461b000231ad8a93d0d9bd474fee2b6aa
Chimwemwe <chimwemwe.kachaje@baobabhealth.org>
2014-11-07 16:45:12 +0200

    Added some missing files

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -187,6 +187,23 @@ class ApplicationController < ActionController::Base
 
   end
 
+  def print_and_redirect(print_url, redirect_url, message = "Printing, please wait...", show_next_button = false, patient_id = nil)
+    @print_url = print_url
+    @redirect_url = redirect_url
+    @message = message
+    @show_next_button = show_next_button
+    @patient_id = patient_id
+    render :template => 'print/print', :layout => nil
+  end
+
+  def print_location_and_redirect(print_url, redirect_url, message = "Printing, please wait...", show_next_button = false, patient_id = nil)
+    @print_url = print_url
+    @redirect_url = redirect_url
+    @message = message
+    @show_next_button = show_next_button
+    render :template => 'print/print_location', :layout => nil
+  end
+
 protected
 
   def authenticate_user
--- app/controllers/patients_controller.rb
+++ app/controllers/patients_controller.rb
@@ -3,7 +3,29 @@ class PatientsController < ApplicationController
   before_filter :find_patient
 
   def show
-    # raise session.to_yaml
+
+    @links = {}
+
+    @task.tasks.each{|task|
+
+      next if task.downcase == "update baby outcome" and (@patient.current_babies.length == 0 rescue false)
+      next if !@task.current_user_activities.include?(task)
+
+      ctrller = "protocol_patients"
+
+      if File.exists?("#{Rails.root}/config/protocol_task_flow.yml")
+
+        ctrller = YAML.load_file("#{Rails.root}/config/protocol_task_flow.yml")["#{task.downcase.gsub(/\s/, "_")}"] rescue ""
+
+      end
+
+      @links[task.titleize] = "/#{ctrller}/#{task.gsub(/\s/, "_")}?patient_id=#{
+      @patient.id}&user_id=#{params[:user_id]}" + (task.downcase == "update baby outcome" ?
+          "&baby=1&baby_total=#{(@patient.current_babies.length rescue 0)}" : "")
+
+    }
+
+    @project = get_global_property_value("project.name") rescue "Unknown"
 
     render :layout => false
   end
