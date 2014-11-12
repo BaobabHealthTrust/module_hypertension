1b63c667c64d687ca832a0cd18181268a0b54c03
Chimwemwe <chimwemwe.kachaje@baobabhealth.org>
2014-11-12 09:34:18 +0200

    Added Core Models

---
+++ app/models/core/encounter_type.rb
@@ -0,0 +1,8 @@
+module Core
+  class EncounterType < ActiveRecord::Base
+    set_table_name :encounter_type
+    set_primary_key :encounter_type_id
+    include Openmrs
+    has_many :encounters, :conditions => {:voided => 0}
+  end
+end
---
+++ app/models/core/global_property.rb
@@ -0,0 +1,12 @@
+module Core
+  class GlobalProperty < ActiveRecord::Base
+    set_table_name "global_property"
+    set_primary_key "property"
+    include Openmrs
+
+    def to_s
+      return "#{property}: #{property_value}"
+    end
+
+  end
+end
