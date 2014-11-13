3eb2ee519386552a19ebbcfe9ee0469c50214bd7
Chimwemwe <chimwemwe.kachaje@baobabhealth.org>
2014-11-13 08:17:15 +0200

    Started adding prescription views but not stable yet

--- app/models/core/concept_set.rb
+++ app/models/core/concept_set.rb
@@ -4,6 +4,6 @@ module Core
     set_primary_key :concept_set_id
     include Core::Openmrs
     belongs_to :set, :class_name => 'Core::Concept', :conditions => {:retired => 0}
-    belongs_to :concept, :conditions => {:retired => 0}
+    belongs_to :concept, :class_name => 'Core::Concept', :conditions => {:retired => 0}
   end
 end
