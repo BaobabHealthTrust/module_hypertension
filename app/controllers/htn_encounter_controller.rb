e36f1913949eb69552421c107da68868eb99df86
mangochiman <mangochiman@gmail.com>
2015-04-22 16:19:44 +0200

    Modifying BP drug management screen

--- app/controllers/htn_encounter_controller.rb
+++ app/controllers/htn_encounter_controller.rb
@@ -434,6 +434,12 @@ class HtnEncounterController < ApplicationController
     @bp_drug_use_history = Observation.find(:last, :conditions => ["person_id =? AND
       concept_id =?", params[:patient_id], Concept.find_by_name('DRUG USE HISTORY').id])
 
+    @bp_treatment_info_available = false
+    bp_treatment_status = Core::Observation.find(:last, :conditions => ["person_id =? AND
+        concept_id =?", params[:patient_id], Concept.find_by_name("TREATMENT STATUS").id])
+    unless bp_treatment_status.blank?
+      @bp_treatment_info_available = true
+    end
   end
  
   def update_htn_provider
